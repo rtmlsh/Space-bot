94a66429fdaa9c9c971999f493dfb10b8e168b51
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2021-08-23 23:53:55 +0300

    Тестовый перенос файла в папку

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import requests
+import os
+import shutil
 
-filename = 'hubble.jpeg'
 url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+path = '/Users/mac/Documents/GitHub/Space-bot/image/'
 
-response = requests.get(url)
-response.raise_for_status()
+def ensure_dir(path):
+    directory = os.path.dirname(path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
-with open(filename, 'wb') as file:
-    file.write(response.content)
+
+
+def get_image(url):
+    filename = 'hubble.jpeg'
+    response = requests.get(url)
+    response.raise_for_status()
+    with open(filename, 'wb') as file:
+        file.write(response.content)
+
+
+destination = os.path.split(path)
+source = get_image(url)
+
+#shutil.move(source, destination)
+print(destination)
