53aed67283dbeb67a4f50afd3243cc934591834d
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2021-08-24 22:22:09 +0300

    Сделал запрос к API для скачивания картинок в папку

--- main.py
+++ main.py
@@ -27,4 +27,9 @@ get_image(url, path, filename)
 url = 'https://api.spacexdata.com/v4/launches/latest'
 response = requests.get(url)
 response.raise_for_status()
-pprint.pprint(response.json()['links']['flickr']['original'])
+spacex_images = response.json()['links']['flickr']['original']
+
+for images_links in enumerate(spacex_images):
+    filename = f'spacex{images_links[0]}.jpg'
+    url = images_links[1]
+    get_image(url, path, filename)
