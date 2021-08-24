49a9334da60fc38d3edcbcd6ba42da7b3e886f19
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2021-08-24 12:31:19 +0300

    Добавил скачивание картинки в папку через функцию

--- main.py
+++ main.py
@@ -4,23 +4,23 @@ import os
 
 url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
 path = '/Users/Алена/Documents/GitHub/Space-bot/images/'
-
+filename = 'hubble.jpeg'
 
 def ensure_dir(path):
     directory = os.path.dirname(path)
     if not os.path.exists(directory):
         os.makedirs(directory)
-        get_image(url)
 
-def get_image(url):
-    filename = '/Users/Алена/Documents/GitHub/Space-bot/images/hubble.jpeg'
+
+def get_image(url, path, filename):
+    image_path = f'{path}{filename}'
     response = requests.get(url)
     response.raise_for_status()
-    with open(filename, 'wb') as file:
+    with open(image_path, 'wb') as file:
         file.write(response.content)
 
 
 ensure_dir(path)
-
+get_image(url, path, filename)
 
 
