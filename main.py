3336f15fac5ff30cf4c0325b5ca9b7df1e0b15d5
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2021-08-24 22:31:48 +0300

    Оформил в функцию запрос к API для скачивания картинок в папку

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import requests
 import os
-import pprint
 
-url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+img_url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
 path = '/Users/mac/Documents/GitHub/Space-bot/images/'
+spacex_api_url = 'https://api.spacexdata.com/v4/launches/latest'
 filename = 'hubble.jpeg'
 
 def ensure_dir(path):
@@ -20,16 +20,18 @@ def get_image(url, path, filename):
         file.write(response.content)
 
 
+def fetch_spacex_last_launch(spacex_api_url):
+    response = requests.get(spacex_api_url)
+    response.raise_for_status()
+    spacex_images = response.json()['links']['flickr']['original']
+
+    for images_links in enumerate(spacex_images):
+        filename = f'spacex{images_links[0]}.jpg'
+        url = images_links[1]
+        get_image(url, path, filename)
+
+
+
 ensure_dir(path)
-get_image(url, path, filename)
-
-
-url = 'https://api.spacexdata.com/v4/launches/latest'
-response = requests.get(url)
-response.raise_for_status()
-spacex_images = response.json()['links']['flickr']['original']
-
-for images_links in enumerate(spacex_images):
-    filename = f'spacex{images_links[0]}.jpg'
-    url = images_links[1]
-    get_image(url, path, filename)
+get_image(img_url, path, filename)
+fetch_spacex_last_launch(spacex_api_url)
