2227b1f08a2c1336ac3a709a270f26c40f6c0cd0
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2021-08-24 21:06:31 +0300

    Сделал запрос к API для отображения картинок

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import requests
 import os
-
+import pprint
 
 url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
-path = '/Users/Алена/Documents/GitHub/Space-bot/images/'
+path = '/Users/mac/Documents/GitHub/Space-bot/images/'
 filename = 'hubble.jpeg'
 
 def ensure_dir(path):
@@ -24,3 +24,7 @@ ensure_dir(path)
 get_image(url, path, filename)
 
 
+url = 'https://api.spacexdata.com/v4/launches/latest'
+response = requests.get(url)
+response.raise_for_status()
+pprint.pprint(response.json()['links']['flickr']['original'])
