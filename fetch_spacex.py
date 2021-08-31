138e09fe02b17cb10cb9b94dcaf05096a6407f90
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2021-08-31 14:16:21 +0300

    Разделил файл main

---
+++ fetch_spacex.py
@@ -0,0 +1,16 @@
+import requests
+
+
+def fetch_spacex_last_launch(spacex_api_url):
+    payloads = {'launch_year': '2018'}
+    response = requests.get(spacex_api_url, params=payloads)
+    response.raise_for_status()
+    spacex_images_links = response.json()[0]['links']['flickr_images']
+    return spacex_images_links
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -6,12 +6,20 @@ import datetime
 import argparse
 from urllib.parse import urlparse
 from dotenv import load_dotenv
+from fetch_spacex import *
 
 
 def ensure_dir(path):
     directory = os.path.dirname(path)
     if not os.path.exists(directory):
         os.makedirs(directory)
+
+
+def get_image_links(spacex_images_links):
+    for image_link in enumerate(spacex_images_links):
+        filename = f'spacex{image_link[0]}.jpg'
+        url = image_link[1]
+        get_image(url, path, filename)
 
 
 def get_image(url, path, filename):
@@ -22,57 +30,57 @@ def get_image(url, path, filename):
         file.write(response.content)
 
 
-def fetch_hubble_photo(path):
-    filename = 'hubble.jpeg'
-    url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
-    get_image(url, path, filename)
+# def fetch_hubble_photo(path):
+#     filename = 'hubble.jpeg'
+#     url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+#     get_image(url, path, filename)
 
 
-def fetch_spacex_last_launch():
-    response = requests.get(spacex_api_url)
-    response.raise_for_status()
-    spacex_images_links = response.json()['links']['flickr']['original']
-    for image_link in enumerate(spacex_images_links):
-        filename = f'spacex{image_link[0]}.jpg'
-        url = image_link[1]
-        get_image(url, path, filename)
+# def fetch_spacex_last_launch():
+#     response = requests.get(spacex_api_url)
+#     response.raise_for_status()
+#     spacex_images_links = response.json()['links']['flickr']['original']
+#     for image_link in enumerate(spacex_images_links):
+#         filename = f'spacex{image_link[0]}.jpg'
+#         url = image_link[1]
+#         get_image(url, path, filename)
 
-
-def fetch_nasa_day_photo():
-    payloads = {"api_key": nasa_token, 'count': 7}
-    response = requests.get(nasa_api_url, params=payloads)
-    response.raise_for_status()
-    for i in range(7):
-        filename = f'nasa{i}{file_extension(response.json()[i]["url"])}'
-        url = response.json()[i]['url']
-        get_image(url, path, filename)
-
-
-def fetch_epic_photo():
-    response = requests.get(nasa_epic_api_url)
-    response.raise_for_status()
-    for i in range(7):
-        date = datetime.datetime.fromisoformat(response.json()[i]['date']).\
-            strftime("%Y/%m/%d")
-        title = response.json()[i]['image']
-        url = 'https://api.nasa.gov/EPIC/archive/natural/' \
-              f'{date}/png/{title}.png?api_key=DEMO_KEY'
-        filename = f'{title}.png'
-        get_image(url, path, filename)
-
-
-def file_extension(img_url):
-    image_path = urlparse(img_url)
-    image_extension = os.path.splitext(os.path.split(image_path.path)[-1])[-1]
-    return image_extension
-
-
-def publish_on_channel():
-    while True:
-        for root, dirs, files in os.walk(path):
-            for filename in files:
-                time.sleep(10)
-                bot.send_photo(chat_id=chat_id, photo=open(f'{path}{filename}', 'rb'))
+#
+# def fetch_nasa_day_photo():
+#     payloads = {"api_key": nasa_token, 'count': 7}
+#     response = requests.get(nasa_api_url, params=payloads)
+#     response.raise_for_status()
+#     for i in range(7):
+#         filename = f'nasa{i}{file_extension(response.json()[i]["url"])}'
+#         url = response.json()[i]['url']
+#         get_image(url, path, filename)
+#
+#
+# def fetch_epic_photo():
+#     response = requests.get(nasa_epic_api_url)
+#     response.raise_for_status()
+#     for i in range(7):
+#         date = datetime.datetime.fromisoformat(response.json()[i]['date']).\
+#             strftime("%Y/%m/%d")
+#         title = response.json()[i]['image']
+#         url = 'https://api.nasa.gov/EPIC/archive/natural/' \
+#               f'{date}/png/{title}.png?api_key=DEMO_KEY'
+#         filename = f'{title}.png'
+#         get_image(url, path, filename)
+#
+#
+# def file_extension(img_url):
+#     image_path = urlparse(img_url)
+#     image_extension = os.path.splitext(os.path.split(image_path.path)[-1])[-1]
+#     return image_extension
+#
+#
+# def publish_on_channel():
+#     while True:
+#         for root, dirs, files in os.walk(path):
+#             for filename in files:
+#                 time.sleep(10)
+#                 bot.send_photo(chat_id=chat_id, photo=open(f'{path}{filename}', 'rb'))
 
 
 if __name__ == '__main__':
@@ -93,8 +101,12 @@ if __name__ == '__main__':
     bot = telegram.Bot(token=telegram_token)
     parser.parse_args()
     ensure_dir(path)
-    fetch_spacex_last_launch()
-    fetch_nasa_day_photo()
-    fetch_hubble_photo()
-    fetch_epic_photo()
-    publish_on_channel()
+    spacex_images_links = fetch_spacex_last_launch(spacex_api_url)
+    get_image_links(spacex_images_links)
+
+
+    # fetch_spacex_last_launch()
+    # fetch_nasa_day_photo()
+    # fetch_hubble_photo()
+    # fetch_epic_photo()
+    # publish_on_channel()
